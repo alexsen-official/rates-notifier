24890a36c64f78d8c4d48e60b947fe1d58d5ed68
alexsen-official <alexsen.official@gmail.com>
2022-07-19 22:47:27 +0300

    minor bugs fixing

--- mongoose.js
+++ mongoose.js
@@ -1,8 +1,13 @@
-require('mongoose').connect(
-    process.env.CONNECTION_STRING,
-    { useNewUrlParser: true,
-      useUnifiedTopology: true },
-    err => {
-        if (err) throw err;
-        console.log('[server]: successfully connected to MongoDB!');
-    });
+const { mongoose } = require('mongoose');
+
+const uri = process.env.CONNECTION_STRING;
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(uri, options, err => {
+    if (err) throw err;
+    console.log('[server]: successfully connected to MongoDB!');
+});
--- server.js
+++ server.js
@@ -3,14 +3,15 @@ require('./mongoose');
 
 const express = require('express'),
       app = express(),
-    
-      path = require('path'),
+
+      cors = require('cors'),
       parser = require('body-parser'),
       routers = require('./routers'),
-      
+      path = require('path'),
+
       PORT = process.env.PORT || 3000;
 
-app.use(require('cors')());
+app.use(cors());
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
@@ -20,9 +21,10 @@ app.use('/rates', routers.RateRouter);
 app.use('/users', routers.UserRouter);
 app.use('/subscriptions', routers.SubscriptionRouter);
 
-app.use(express.static(path.resolve(__dirname, './dist/frontend')));
+app.use(express.static(path.join(__dirname, 'dist/frontend')));
+
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname))
+    res.sendFile(path.join(__dirname, 'dist/frontend/index.html'))
 );
 
 app.listen(PORT, err => {
